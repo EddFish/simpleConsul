11d40a946def47ea8ada0c05282a4825fa2a0c2e
EddFish <edwardlfish@gmail.com>
2020-04-25 15:07:44 +0300

    First commit

---
+++ src/main/java/com/telran/HerokuPluginApplication.java
@@ -0,0 +1,13 @@
+package com.telran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuPluginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuPluginApplication.class, args);
+	}
+
+}
